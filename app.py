b1a69487c2f0aba4d3f24e438bfb9f24a85e7abb
Artemiy <artemiyyparamonov@gmail.com>
2024-12-16 01:11:38 +0300

    Pages

--- app.py
+++ app.py
@@ -2,30 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('Graphs.')
-c1, c2 = st.columns(2)
-c3, c4 = st.columns(2)
+if "data" not in st.session_state:
+    st.session_state.data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
+st.sidebar.success("Select a demo above.")
+
+st.bar_chart(st.session_state.data)
+button1, button2 = st.columns(2)
 with st.container():
-    c1.write("c1")
-    c2.write("c2")
+    file = button1.file_uploader(label="Upload log file")
+    if file is not None:
+        st.session_state.data = LogFileParser(StringIO(file.getvalue().decode("utf-8")))
+        st.write(parser.parse('{Data} --- {Message}'))
+    text = button2.text_input("Log Format")
 
-with st.container():
-    c3.write("c3")
-    c4.write("c4")
 
-with c1:
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-    st.area_chart(chart_data)
 
-with c2:
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-    st.bar_chart(chart_data)
 
-with c3:
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-    st.line_chart(chart_data)
-
-with c4:
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-    st.line_chart(chart_data)
